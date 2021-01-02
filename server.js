64965da37076285b3736ca05ed10ae3dd610b4da
Pranish VD <pranishveliyath666@gmail.com>
2021-01-02 21:26:37 +0530

    added Mongogb connection

--- server.js
+++ server.js
@@ -1,23 +1,29 @@
-const express = require('express');
-
-const dotenv  = require('dotenv');
-const route = require('./routes/routes');
-
-const morgan = require('morgan');
-
+const express = require("express");
+const dotenv = require("dotenv");
+const route = require("./routes/routes");
+const morgan = require("morgan");
+const connectDB = require("./config/db.config");
 
 // load env File
 
-dotenv.config({path:'./config/config.env'});
-const app = express()
-
 //use middleware morgan
+// app.use(morgan("dev"));
+dotenv.config({ path: "./config/config.env" });
+const app = express();
 
-app.use(morgan('dev'));
+connectDB();
 
-app.use('/api/v1/bootcamp',route);
+app.use("/api/v1/bootcamp", route);
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(
+  PORT,
+  console.log(`Server runing in ${process.env.NODE_ENV} on port ${PORT}`)
+);
 
 
-const PORT = process.env.PORT||5000
-
-app.listen(PORT, console.log(`Server runing in ${process.env.NODE_ENV} on port ${PORT}`));
+process.on('unhandledRejection',(err,promise)=>{
+  console.log(`The Error is : ${err}`)
+  server.close(()=>process.exit(1))
+})
